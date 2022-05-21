95ae01ac9fe819543cbec7c675c8d0e76c4721ae
hermannm <hermann.morkrid@gmail.com>
2022-05-21 14:30:13 +0200

    add powershell profile

---
+++ Documents/WindowsPowerShell/profile.ps1
@@ -0,0 +1,26 @@
+Remove-Module PSReadline
+
+# Customizes the prompt.
+function Prompt {
+    $color = [char]27
+
+    $path = switch -Wildcard ($executionContext.SessionState.Path.CurrentLocation.Path) {
+        "$HOME" { "~" }
+        "$HOME\*" { $executionContext.SessionState.Path.CurrentLocation.Path.Replace($HOME, "~").Replace("\","/") }
+        default { $executionContext.SessionState.Path.CurrentLocation.Path.Replace("\","/") }
+    }
+
+    $branch = git branch --show-current
+    $branchString = ""
+    if ($branch) {
+        $branchString = "$color[37m/$color[1;33m$branch"
+    }
+
+    return "$color[1;34m$path$branchString$color[37m: "
+}
+
+# Utility function for handling dotfiles with Git.
+function Git-Dotfiles {
+    git --git-dir="$HOME/dotfiles" --work-tree="$HOME" $args
+}
+Set-Alias -Name dotfiles -Value Git-Dotfiles
