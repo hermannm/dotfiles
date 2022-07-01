cf822097dc3cdac02d89ec43d5700bb7a94070a0
hermannm <hermann.morkrid@gmail.com>
2022-07-01 22:35:58 +0200

    refactor looping in sort-git-status util script

--- util-scripts/sort-git-status.py
+++ util-scripts/sort-git-status.py
@@ -7,16 +7,16 @@ import re
 def make_order_dict() -> dict[str, int]:
     order = {"##": 0}
     statuses = ["M", "A", "C", "R", "D", "U"]
+    status_len = len(statuses)
 
     for (i, s) in enumerate(statuses):
         order[f"{s} "] = i + 1
-        order[f" {s}"] = i + 1 + len(statuses)
+        order[f" {s}"] = i + 1 + status_len
 
-    for s1 in statuses:
-        for s2 in statuses:
-            order[f"{s2}{s1}"] = len(order)
+        for (j, s2) in enumerate(statuses):
+            order[f"{s2}{s}"] = (i + 2) * status_len + j + 1
 
-    order["??"] = len(order)
+    order["??"] = (2 + status_len) * status_len + 2
 
     return order
 
@@ -24,4 +24,4 @@ def make_order_dict() -> dict[str, int]:
 order = make_order_dict()
 ansi_re = re.compile(r"\x1b[^m]*m")
 
-print("".join(sorted(sys.stdin.readlines(), key=lambda line: order.get(ansi_re.sub("", line)[0:2], 0))))
+print("".join(sorted(sys.stdin.readlines(), key=lambda line: order.get(ansi_re.sub("", line)[0:2], 0))), end="")
