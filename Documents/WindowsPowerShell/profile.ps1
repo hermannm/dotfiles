05aa365ae3728c3686c4c8a5bd50cdaa19e3be80
hermannm <hermann.morkrid@gmail.com>
2022-08-03 00:19:32 +0200

    change dotfiles function to take full git command as args

--- Documents/WindowsPowerShell/profile.ps1
+++ Documents/WindowsPowerShell/profile.ps1
@@ -20,12 +20,18 @@ function prompt {
     return "$color[1;34m$path$branchString$color[37m`$$color[00m "
 }
 
-# Utility function for handling dotfiles with Git.
+# Utility function for handling dotfiles with git.
 function dotfiles {
-    git --git-dir="$HOME/dotfiles" --work-tree="$HOME" $args
+    $env:GIT_DIR = "$HOME/dotfiles"
+    $env:GIT_WORK_TREE = "$HOME"
+
+    $(& $args[0] $args[1..($args.count - 1)])
+
+    remove-item env:\GIT_DIR
+    remove-item env:\GIT_WORK_TREE
 }
 
-# Utility function for viewing dotfiles added to Git.
+# Utility function for viewing dotfiles added to git.
 function dotfiles-ls {
-    dotfiles ls-tree windows -r --name-only
+    dotfiles git ls-tree windows -r --name-only
 }
