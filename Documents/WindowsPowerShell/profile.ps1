89f321548f6c9ceb98cf9ac263d50b2882e52a57
hermannm <hermann.morkrid@gmail.com>
2022-11-04 13:30:55 +0100

    improve consistency of powershell profile syntax

--- Documents/WindowsPowerShell/profile.ps1
+++ Documents/WindowsPowerShell/profile.ps1
@@ -4,11 +4,11 @@ function prompt {
     $gray = "${color}[22;37m"
     $blue = "${color}[1;34m"
     $purple = "${color}[1;35m"
-    $default = "${color}[00m"
+    $reset_color = "${color}[00m"
 
     $path = switch -Wildcard ($executionContext.SessionState.Path.CurrentLocation.Path) {
-        "$HOME" { "~" }
-        "$HOME\*" { $executionContext.SessionState.Path.CurrentLocation.Path.Replace($HOME, "~").Replace("\","/") }
+        "${HOME}" { "~" }
+        "${HOME}\*" { $executionContext.SessionState.Path.CurrentLocation.Path.Replace($HOME, "~").Replace("\","/") }
         default { $executionContext.SessionState.Path.CurrentLocation.Path.Replace("\","/") }
     }
 
@@ -18,7 +18,7 @@ function prompt {
         $branchString = "${gray}:${purple}${branch}"
     }
 
-    return "${blue}${path}${branchString}${gray}`$${default} "
+    return "${blue}${path}${branchString}${gray}`$${reset_color} "
 }
 
 # Changes ls command to use ls from Git, using Linux style.
@@ -30,12 +30,12 @@ if ($host.Name -eq 'ConsoleHost')
 
 # Configures auto-complete suggestions.
 import-module PSReadline
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-Set-PSReadLineOption -ShowToolTips
-Set-PSReadLineOption -PredictionSource History
+set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+set-PSReadLineOption -ShowToolTips
+set-PSReadLineOption -PredictionSource History
 
 # Disables syntax highlighting.
-Set-PSReadlineOption -Colors @{
+set-PSReadlineOption -Colors @{
     ContinuationPrompt = "white"
     Emphasis = "white"
     Error = "white"
@@ -58,41 +58,41 @@ Set-PSReadlineOption -Colors @{
 $global:psprofile = "${HOME}\Documents\WindowsPowerShell\profile.ps1"
 
 # Shortcut for Docker.
-function dc { docker compose $args }
+function dc { docker compose ${args} }
 
 # Shortcuts for Jetbrains IDEs.
-function rider { rider64 $args }
-function idea { idea64 $args }
+function rider { rider64 ${args} }
+function idea { idea64 ${args} }
 
 # Remove existing aliases to be overridden by our git shortcuts below.
 del alias:gc -force
 del alias:gps -force
 
 # Shortcuts for common git operations.
-function g { git $args }
-function ga { git add $args }
-function gb { git branch $args }
-function gc { git commit $args }
-function gca { git add . ; git commit $args }
-function gch { git checkout $args }
+function g { git ${args} }
+function ga { git add ${args} }
+function gb { git branch ${args} }
+function gc { git commit ${args} }
+function gca { git add . ; git commit ${args} }
+function gch { git checkout ${args} }
 function gchm { git checkout $(git-main-branch) }
-function glg { git log --oneline $args }
-function gpl { git pull $args }
+function glg { git log --oneline ${args} }
+function gpl { git pull ${args} }
 function gplom { git pull origin $(git-main-branch) }
-function gps { git push }
-function gpsu { git push -u origin $(git-current-branch) $args }
-function grh { git reset --hard origin/$(git-current-branch) }
-function gs { git -c color.ui=always status -sb | python "$HOME/util-scripts/sort-git-status.py" }
+function gps { git push ${args} }
+function gpsu { git push -u origin $(git-current-branch) ${args} }
+function grh { git reset --hard origin/$(git-current-branch) ${args} }
+function gs { git -c color.ui=always status -sb ${args} | python "$HOME/util-scripts/sort-git-status.py" }
 
 # Shortcuts for git management of dotfiles.
-function df-g { git --git-dir="$HOME/dotfiles" --work-tree="$HOME" $args }
-function df-ga { df-g add $args }
-function df-gb { df-g branch $args }
-function df-gc { df-g commit $args }
-function df-glg { df-g log --oneline $args }
-function df-gpl { df-g pull $args }
-function df-gps { df-g push $args }
-function df-gs { df-g -c color.ui=always status -sb | python "$HOME/util-scripts/sort-git-status.py" }
+function df-g { git --git-dir="$HOME/dotfiles" --work-tree="$HOME" ${args} }
+function df-ga { df-g add ${args} }
+function df-gb { df-g branch ${args} }
+function df-gc { df-g commit ${args} }
+function df-glg { df-g log --oneline ${args} }
+function df-gpl { df-g pull ${args} }
+function df-gps { df-g push ${args} }
+function df-gs { df-g -c color.ui=always status -sb ${args} | python "$HOME/util-scripts/sort-git-status.py" }
 function df-ls { df-g ls-tree windows -r --name-only }
 
 # Returns the current git branch.
@@ -103,9 +103,9 @@ function git-main-branch {
     if ((git rev-parse --git-dir 2>$null) -eq $null) { return }
 
     $main_refs = "refs/heads/main", "refs/remotes/origin/main"
-    foreach ($ref in $main_refs) {
-        git show-ref -q --verify $ref
-        if ($?) {
+    foreach ($ref in ${main_refs}) {
+        git show-ref -q --verify ${ref}
+        if (${?}) {
             return "main"
         }
     }
@@ -116,12 +116,13 @@ function git-main-branch {
 # Enters Python virtual environment in provided path.
 function venv {
     $venv_path = "venv\scripts\activate"
-    if ($args.Count -eq 0) {
-        . .\$venv_path
+    if (${args}.Count -eq 0) {
+        . .\${venv_path}
     } else {
         $target_dir = "$($args[0])"
-        . .\$target_dir\$venv_path
+        . .\${target_dir}\${venv_path}
     }
 }
 
+# Sets Django env variable for indok-web project.
 $env:DJANGO_READ_DOT_ENV_FILE = "true"
